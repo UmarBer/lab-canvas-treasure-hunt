91d3f425c735af7ccc963e564a7e7f721f1aa404
Umar Bermejo <umarber94@gmail.com>
2022-02-12 22:10:17 +0100

    Iteration 3 done too

--- main.js
+++ main.js
@@ -38,27 +38,46 @@ function drawGrid() {
 }
 
 // Character creation
-class Character {
-  constructor(col, row) {
-    this.col = col;
-    this.row = row;
+function drawPlayer(col, row) {
+  class Character {
+    constructor(col, row) {
+      this.col = col;
+      this.row = row;
+    }
+    moveUp() {
+      this.row -= row;
+    }
+    moveRight() {
+      this.col += col;
+    }
+    moveDown() {
+      this.row += row;
+    }
+    moveLeft() {
+      this.col -= col;
+    }
   }
-  moveUp() {
-    this.row -= 1;
+  const player = new Character(0, 0);
+
+  // Drawing the player
+  class Player extends Character {
+    constructor(col, row) {
+      super(col, row);
+      this.col = col;
+      this.row = row;
+    }
+    drawOfPlayer() {
+      const playerImg = new Image();
+      playerImg.src = 'images/character-down.png';
+      context.drawImage(playerImg, this.col, this.row, 50, 50);
+    }
   }
-  moveRight() {
-    this.col += 1;
-  }
-  moveDown() {
-    this.row += 1;
-  }
-  moveLeft() {
-    this.col -= 1;
-  }
+  let newPlayer = new Player(col, row);
+  newPlayer.drawOfPlayer(col, row);
 }
 
+drawPlayer(100, 200);
 /*
-const player = new Character(0, 0); // (0,0) = Initial position
 
 player.moveDown(); // Increase by 1 the value of player.row
 player.moveDown(); // Increase by 1 the value of player.row
